580110039ed0c2ed2d995e1092c22dd2fb35c396
triandre813 <159310616+triandre813@users.noreply.github.com>
2024-03-22 15:30:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,110 @@
+#set a = 1
+a = 1
+#set b = 2
+b = 2
+#set c = 3
+c = 5
+while a < c:
+  #increment a by 1
+  a = a + 1
+  #add the value of c to b  
+  b = b + c
+#endwhile
+
+#print the final value of a and b
+print("Final values a =", a, ", b =", b)
+
+
+#set d = 4
+d = 4
+#set e = 6
+e = 6
+#set f = 7
+f = 7
+while d > f:
+  #increment d by 1
+  d = d + 1
+  #decrement e by 1
+  e = e - 1
+#endwhile
+
+#print the final value of d, e and f
+print("Final values d =", d, ", e =", e, ", f =", f)
+
+
+#set g = 4
+g = 4
+#set h = 6
+h = 6
+while g < h:
+  #increment g by 1
+  g = g + 1
+
+#print the final value of g and h
+print("Final values g =", g, ", h =", h)
+
+
+#set j = 2
+j = 2
+#set k = 5
+k = 5
+#set n = 9
+n = 9
+while j < k:
+  #set m = 6
+  m = 6
+  while m < n:
+     #print "Goodbye"
+     print("Goodbye")
+    #increment m by 1
+     m = m + 1
+    #increment j by 1
+  j = j + 1
+  #print j and k
+  print("j =", j, ", k =", k)
+
+
+#set j = 2
+j = 2
+#set k = 5
+k = 5
+#set m = 6 
+m = 6
+#set n = 9
+n = 9
+while j < k:
+  while m < n:
+    #print "Hello"
+    print("Hello")
+    #increment m by 1
+    m = m + 1
+    #increment j by 1
+  j = j + 1
+  #print j, k, m and n
+  print("j =", j, ", k =", k, ", m =", m, ", n =", n)
+
+
+#set p = 2
+p = 2
+#set q = 4
+q = 4
+while p < q:
+  #print "Adios"
+  print("Adios")
+  #set r = 1
+  r = 1
+  while r < q:
+    #print "Adios"
+    print("Adios")
+    #increment r by 1
+    r = r + 1
+    #increment p by 1
+  p = p + 1
+  #print p, q and r
+  print("p =", p, ", q =", q, ", r =", r)
+  
+  
+        
+ 
+        
+
